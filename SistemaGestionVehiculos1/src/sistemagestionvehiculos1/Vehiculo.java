3ccfd93f7f1c4af2671fc6c7b1ca6ea3eda351c7
LAB-USR-CHIMBOTE <LAB-USR-CHIMBOTE@grupoutp.net>
2024-10-11 16:02:04 -0500

    ALEXANDER LOZANO

---
+++ SistemaGestionVehiculos1/src/sistemagestionvehiculos1/Vehiculo.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package sistemagestionvehiculos1;
+
+/**
+ *
+ * @author LAB-USR-CHIMBOTE
+ */
+public class Vehiculo {
+
+  
+    
+}
